30420b518a78783a98fbdc88cd51c7ae203fafbc
MadukaJustice <jonasjustice98@gmail.com>
2022-09-13 06:58:05 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alpha followed by a new line
+ *
+ * Return: Always 0 (Success
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+
+	_putchar('\n');
+}
